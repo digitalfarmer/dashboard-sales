32ef52667ceee336ff488a8a8281bc51b3fc2a52
Ades <37319154+digitalfarmer@users.noreply.github.com>
2026-02-11 09:13:22 +0700

    create deployment in docker environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Target standalone build
 };
 
 export default nextConfig;
